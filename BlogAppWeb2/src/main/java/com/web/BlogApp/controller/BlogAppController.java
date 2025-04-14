97f8d0439336fb27957a517fc79301716e7c383d
GuilhermeAntonio05 <guilherzer4@gmail.com>
2025-04-13 23:52:52 -0300

    adicionada função de adicionar comentários

--- BlogAppWeb2/src/main/java/com/web/BlogApp/controller/BlogAppController.java
+++ BlogAppWeb2/src/main/java/com/web/BlogApp/controller/BlogAppController.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -69,8 +71,16 @@ public class BlogAppController {
 		return "redirect:/posts";
 	}
 	
+	@GetMapping(value = "/newpostcomment/{id}")
+	public String getPostComment() {
+		return "newComment";
+	}
+	
 	@PostMapping(value = "/newpostcomment/{id}")
-	public String savePost(@PathVariable(name = "id") UUID id,@RequestBody @Valid PostCommentModel post, BindingResult result, RedirectAttributes attributes) {
+	public String savePostComment(@PathVariable("id") UUID id, @ModelAttribute PostCommentModel post, BindingResult result, RedirectAttributes attributes) {
+
+		System.out.println(post.getComentario());
+		
 		if (result.hasErrors()) {
 			attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos");
 			return "redirect:/newpostcomment";
